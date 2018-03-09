f69073891878b1f22f0991cffa34a053de59fd6e
Sam Maggay <maggay.sam@gmail.com>
2018-03-09 18:56:03 +0800

    Mar08-live

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,19 @@
-// Init Slider
-$('.slider').slider();		
+$(document).ready(function(){
+  // Init Carousel
+  $('.carousel').carousel();
+
+  // Init Carousel Slider
+  $('.carousel.carousel-slider').carousel({fullWidth:true});
+
+  // Fire off toast
+  //Materialize.toast('Hello World', 3000);
+
+  // Init Slider
+  $('.slider').slider();
+
+  // Init Modal
+  $('.modal').modal();
+
+  // Init Sidenav
+  $('.button-collapse').sideNav();
+});
